3876e383112d0094073c91a3fc9be213ea04592d
Ferdyya <ferdyya@gmail.com>
2021-07-30 01:38:46 +0200

    new div using color

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import "tailwindcss/tailwind.css"
-
+import Content from './scripts/Content';
 
 function App() {
   return (
@@ -8,6 +8,11 @@ function App() {
       <h1>
         Hello World
       </h1>
+
+      <Content bgcolor="bg-red-300"/>
+      <Content bgcolor="bg-red-300"/>
+      <Content bgcolor="bg-red-300"/>
+
     </div>
   );
 }
