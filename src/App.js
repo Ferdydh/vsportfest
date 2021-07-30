e1c9cb7a78e196b2d16aeb6bc7eb35119f168779
Ferdyya <ferdyya@gmail.com>
2021-07-30 02:23:34 +0200

    new sidebar, applied tailwind css, most simplistic layout done

--- src/App.js
+++ src/App.js
@@ -9,10 +9,26 @@ function App() {
         Hello World
       </h1>
 
-      <Content bgcolor="bg-red-300"/>
-      <Content bgcolor="bg-red-300"/>
-      <Content bgcolor="bg-red-300"/>
+      <div style={{width: "20%", float:"left"}}>
+        {/*
+        maybe we have to change the href to javascript for better transitions etc? 
+        */}
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <a href="#pembukaan">loncat ke pembukaan</a>
+        </button>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <a href="#isi">loncat ke isi</a>
+        </button>
+        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <a href="#penutup">loncat ke penutup</a>
+        </button>
+      </div>
 
+      <div style={{width: "80%", float:"right"}}>
+        <Content bgcolor="bg-red-300" id="pembukaan" />
+        <Content bgcolor="bg-yellow-300" id="isi" />
+        <Content bgcolor="bg-green-300" id="penutup" />
+      </div>
     </div>
   );
 }
--- src/scripts/Content.js
+++ src/scripts/Content.js
@@ -7,9 +7,10 @@ class Content extends React.Component {
 
     render() {
       const bgcolor = this.props.bgcolor;
+      const id = this.props.id;
 
       return (
-        <div class={bgcolor}>
+        <div class={bgcolor} id={id} >
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
         </div>
