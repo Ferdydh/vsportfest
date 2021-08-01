c0d7cefc4946bbc0a4229702f56b67663fae49a0
alvandi <geradylanalvandi@gmail.com>
2021-08-01 21:10:15 +0200

    Fixed lagging button

--- src/scripts/Sidebar.js
+++ src/scripts/Sidebar.js
@@ -6,37 +6,34 @@ class Sidebar extends React.Component {
     render() {
       return (
         <div class="sidebar">
-            <div> 
-                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-
+            <div>
+            
                 <Link
                 activeClass="active"
                 to="pembukaan"
                 spy={true}
                 smooth={true}
                 offset={-70}
-                duration={500}
+                duration={500} 
                 >
-
-                <a href="#pembukaan">1</a>
-
-                </Link>
-
-                </button>
                 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                1
+                </button>
+                </Link>
+                
                 <Link
                 activeClass="active"
                 to="isi"
                 spy={true}
                 smooth={true}
                 offset={-70}
-                duration={500}
+                duration={500} 
                 >
-                <a href="#isi">2</a>
-                </Link>
-                </button>
-                
                 <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                2
+                </button>
+                </Link>
+                
                 <Link
                 activeClass="active"
                 to="penutup"
@@ -45,9 +42,10 @@ class Sidebar extends React.Component {
                 offset={-70}
                 duration={500}
                 >
-                <a href="#penutup">3</a>
-                </Link>
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                3
                 </button>
+                </Link>
             </div>
         </div>
 
