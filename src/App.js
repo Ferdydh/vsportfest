8efb39732d3859b20164d09e60f665a1babf1d68
alvandi <geradylanalvandi@gmail.com>
2021-08-05 11:03:25 +0200

    Added new Scroll To Top button

--- src/App.js
+++ src/App.js
@@ -4,18 +4,22 @@ import Content from './scripts/Content';
 import Sidebar from './scripts/Sidebar';
 import Contact from './scripts/Contact';
 import Topbar from './scripts/Topbar'
+import ScrollToTopBtn from './scripts/ScrollToTopBtn';
 
 function App() {
   return (
     <section className="App">
+      <ScrollToTopBtn className="fixed"/>
       <Topbar />
 
       <Sidebar />
+      
       <section class="main">
         <Content className="bg-red-300" id="pembukaan" />
         <Content className="bg-yellow-300" id="isi" />
         <Content className="bg-green-300" id="penutup" />
         <Contact />
+        
       </section>
     </section>
   );
---
+++ src/scripts/ScrollToTopBtn.js
@@ -0,0 +1,48 @@
+import React, {Component} from "react";
+
+export default class ScrollToTopBtn extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            is_visible: false
+        };
+    }
+
+    componentDidMount() {
+        var scrollComponent = this;
+        document.addEventListener("scroll", function(e) {
+            scrollComponent.toggleVisibility();
+        })
+    }
+    
+    toggleVisibility() {
+        if(window.pageYOffset > 300) {
+            this.setState({
+                is_visible : true
+            });
+        } else {
+            this.setState({
+                is_visible: false
+            });
+        }
+    }
+    scrollToTop() {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    }
+
+    render() {
+        const { is_visible } = this.state;
+        return (
+            <div className="scroll-to-top">
+                {is_visible && (
+                    <div onClick={() => this.scrollToTop()}>
+                        <img src='https://i.postimg.cc/44Ytsk8Z/top-arrow-emoj.png' alt='Go to top' />
+                    </div>
+                )}
+            </div>
+        );
+    }
+}
