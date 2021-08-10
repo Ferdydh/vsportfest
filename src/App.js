c85bec24b57a892e25d65489be04c400f26a87cd
Ferdyya <ferdyya@gmail.com>
2021-08-10 14:53:51 +0200

    apa sih dilan

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <MainWindow />
       {/* The topmost slide gets extra padding due to the first slide*/}
 
-      <Content className="bg-red-300 p-10" id="pembukaan" />
+      <MoLeg className="bg-red-300 p-10" id="pembukaan" />
 
       <Content className="bg-yellow-300 p-10" id="isi" />
 
