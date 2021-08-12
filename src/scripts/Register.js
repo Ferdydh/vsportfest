071a1bcf362fe3d3fc4879fa3b04ef472ec7d5d0
crustypluto19 <evanchristopher1907@gmail.com>
2021-08-12 17:25:32 +0200

    style changes

--- src/scripts/Register.js
+++ src/scripts/Register.js
@@ -1,30 +1,28 @@
 import React from 'react';
-import { Parallax } from 'react-parallax';
-import BackgroundShape from '../images/shape.png';
+import Countdown from './Countdown'
 import VRMascot from '../images/vr-mascot.png';
+
 
 class Register extends React.Component {
     render() {
       return (
-        <Parallax bgImage={BackgroundShape} className="bg-gradient-to-br from-pink-500 to-blue-200" strength={300}>
-          <div className={this.props.className} id={this.props.id}>
-                  <div className="flex flex-col h-screen justify-center items-center relative ">
-                  <div className="flex w-auto m-5 bg-yellow-300 rounded-lg justify-center items-center">
-                      <div className="text-2xl md:text-4xl p-5">
-                          <a href="google.com">Register now!</a>
-                      </div>
-                      
-                  <div className="bg-purple-300 text-lg p-1">
-                      Frist: date
-                  </div>
-                  <div>Date</div>
-
-                  </div>
-                  <div className="text-xl text-white p-10">Contact person: Pepe julian</div>
+          <div className={this.props.className + " h-screen flex flex-auto relative justify-center content-center bg-gradient-to-br from-blue-900 via-pink-400 to-purple-500 p-10"} id={this.props.id}>
+            <div className="flex flex-wrap container justify-center content-center">
+              <div className="transform -skew-y-6">
+                <div className="text-3xl p-3 m-3 bg-yellow-300 rounded-lg justify-center items-center font-bold animate-bounce ">
+                  <a href="https://www.w3schools.com/" target="_blank">Register now!</a>
                 </div>
+                <div className="animate-pulse text-xl text-red-700">
+                  <Countdown />
+                </div>
+                <div className="text-white">
+                  CP: Pepe
+                </div>
+              </div>
+            
+            <img src={VRMascot} className="object-contain md:w-3/8 h-3/4" alt="Vr mascot"></img>
+            </div>
           </div>
-        </Parallax>
-        
       ); 
     }
 }
