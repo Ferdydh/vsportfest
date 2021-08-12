7e977cb5eaca64e26d2b7266ec1fd01af7598533
alvandi <geradylanalvandi@gmail.com>
2021-08-12 23:54:34 +0200

    Implemented working countdown

--- src/scripts/Countdown.js
+++ src/scripts/Countdown.js
@@ -1,12 +1,52 @@
-import React,{useEffect} from 'react';
+import React,{useEffect , useState} from 'react';
 import '../index.css';
 
-export default class Countdown extends React.Component {
-    render(){
-        return (
-            <div>
-                HELLO WORLD
-            </div>
-        );
-    }
-} 
+function Countdown() {
+    const calculateTimeLeft = () => {
+      let year = new Date().getFullYear();
+      const difference = +new Date(`${year}-08-15`) - +new Date();
+      let timeLeft = {};
+  
+      if (difference > 0) {
+        timeLeft = {
+          days: Math.floor(difference / (1000 * 60 * 60 * 24)),
+          hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+          minutes: Math.floor((difference / 1000 / 60) % 60),
+          seconds: Math.floor((difference / 1000) % 60),
+        };
+      }
+  
+      return timeLeft;
+    };
+  
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+    const [year] = useState(new Date().getFullYear());
+  
+    useEffect(() => {
+      setTimeout(() => {
+        setTimeLeft(calculateTimeLeft());
+      }, 1000);
+    });
+  
+    const timerComponents = [];
+  
+    Object.keys(timeLeft).forEach((interval) => {
+      if (!timeLeft[interval]) {
+        return;
+      }
+  
+      timerComponents.push(
+        <div className="font-extrabold text-3xl font-sans transform skew-x-6 text-white inline-block">
+          {timeLeft[interval]} {interval}{" "}
+        </div>
+      );
+    });
+    return (
+      <div>
+        <h1 className="font-semibold text-xl font-sans transform -skew-x-6 text-white"> Get Ready for the Website Deadline!! </h1>
+        {timerComponents.length ? timerComponents : <span>Time's up!</span>}
+      </div>
+    );
+  }
+  
+  export default Countdown;
