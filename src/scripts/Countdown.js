69e4b5810f6ab8595226ed60b2b7688e0ec47386
alvandi <geradylanalvandi@gmail.com>
2021-08-12 14:55:05 +0200

    added countdown

---
+++ src/scripts/Countdown.js
@@ -0,0 +1,12 @@
+import React,{useEffect} from 'react';
+import '../index.css';
+
+export default class Countdown extends React.Component {
+    render(){
+        return (
+            <div>
+                HELLO WORLD
+            </div>
+        );
+    }
+} 
