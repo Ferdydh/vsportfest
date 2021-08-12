66926a42ea904d9921289b21b4b08c3c40837134
crustypluto19 <evanchristopher1907@gmail.com>
2021-08-13 00:59:27 +0200

    style changes

--- src/scripts/Register.js
+++ src/scripts/Register.js
@@ -9,10 +9,10 @@ import VRMascot from '../images/vr-mascot.png';
 class Register extends React.Component {
     render() {
       return (
-          <section className={this.props.className + " h-screen flex flex-auto relative justify-center content-center bg-gradient-to-br from-pink-400 via-blue-700 to-purple-500 p-10"} id={this.props.id}>
-            <div className="flex flex-wrap container justify-center content-center mt-40 md:mt-20">
-              <div className="transform -skew-y-6">
-                <div className="text-5xl font-extrabold font-mono p-3 m-3 rounded-lg justify-center items-center animate-bounce " style={{backgroundColor: "#EEC184"}}>
+          <section className={"h-auto md:h-screen flex flex-auto relative justify-center content-center bg-gradient-to-br from-pink-400 via-blue-700 to-purple-500 p-10"} id={this.props.id}>
+            <div className="flex flex-wrap container justify-center content-center mt-40 md:mt-10">
+              <div className="transform -skew-y-6 mr-4">
+                <div className="text-5xl font-extrabold font-mono p-3 m-3 rounded-lg justify-center items-center animate-bounce" style={{backgroundColor: "#EEC184"}}>
                   <a href="https://www.w3schools.com/" target="_blank">Register now!</a>
                 </div>
                 <div className="animate-pulse text-2xl text-red-600 font-bold font-mono border-2 border-double p-3">
@@ -36,7 +36,7 @@ class Register extends React.Component {
 
               </div>
             
-            <img src={VRMascot} className="m-5 ml-0 object-contain md:w-3/8 h-3/4 animate-wiggle" alt="Vr mascot"></img>
+            <img src={VRMascot} className="m-5 ml-0 object-contain md:w-3/8 md:h-3/4 animate-wiggle" alt="Vr mascot"></img>
             </div>
           </section>
       ); 
