a83810c27d7f6548b1347a1b84d302cbba9b83e3
alvandi <geradylanalvandi@gmail.com>
2021-08-13 23:58:02 +0200

    Made major changes to content

--- src/scripts/Content.js
+++ src/scripts/Content.js
@@ -1,13 +1,27 @@
 import React,{useEffect} from 'react';
-import Popup from './Popup'
-import Pikachu from '../images/pikachu.jpg'
-import VR from '../images/vr.png'
+
+import Popup from './Popup';
+import Prizes from './Prizes';
+
+import Pikachu from '../images/pikachu.jpg';
+import VR from '../images/vr.png';
 import { Parallax } from 'react-parallax';
+import Modal from './Modal';
+import MascotDota from '../images/pikachu.jpg';
+import FG from '../images/MOLEG.png';
+import BG from '../images/DOTAWALLPAPER.jpg';
+import ICON from '../images/pubg.png';
+
 
 class Content extends React.Component {
 
-  componentDidMount(){
-    console.log("Hello");
+  constructor(props){
+    var visibleBG = "";
+    if(this.props.background == "on") {
+      visibleBG = "BG"
+    } else {
+      visibleBG = ""
+    }
   }
 
   MouseOver(event) {
@@ -24,43 +38,44 @@ class Content extends React.Component {
   render() {
 
     return (
-      <Parallax bgImage={VR} className="bg-gray-500" strength={300}>
+      <Parallax bgImage={BG} className=" bg-purple-900 blur" strength={-500} bgClassName="opacity-30 blur" blur={{ min: 2, max: 4 }}>
+      <Parallax bgImage={FG} strength={300} bgClassName="ml-80 p-60 opacity-80 " blur={{ min: 0, max: 1 }}>
+      <div className="float-right h-20 w-20 p-2">
+        <img src={ICON} />
+      </div>
       <section className="h-screen px-20 md:py-20 flex flex-row " id={this.props.id}>
+
         {/* LEFT SIDE OF THE DIV */}
         <div className="h-full w-1/2 flex flex-col" id="leftside">
-          <div className="m-auto font-bold text-5xl">
-            <div className="transform -skew-y-6 transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-110">
-                MOBILE LEGENDS
+          <div className="m-auto font-bold text-7xl">
+            <div className="transform -skew-y-6 transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-110 text-white select-none">
+              {this.props.sectionName}
             </div>
-            <button className="transform skew-y-6 my-10 float-left transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-110
-             bg-blue-500 font-light hover:bg-blue-700 text-white py-2 px-4 border border-blue-700 rounded text-sm shadow-lg">
-                RULES
-
-            </button>
+              <Modal title={this.props.sectionName} text={this.props.sectionText}/>
           </div>
         </div>
 
         {/* LINE THROUGH MIDDLE OF COMPONENT */}
-        <div className="flex flex-col" id="columnspace">
-        <div className="w-1 h-full">
+        <div className="flex flex-col select-none" id="columnspace">
+        <div className="w-1 h-1/6">
           </div>
-          <div className="bg-gray-800 w-1 h-full">
+          <div className=" bg-white w-1 h-full">
           </div>
-          <div className=" w-1 h-full">
+          <div className=" w-1 h-1/6">
           </div>
         </div>
         
         {/* RIGHT SIDE OF THE DIV */}
-        <div className="h-full w-1/2 flex-col flex" id="rightside">
-        
-          <div className="p-10 m-auto">
-
-              <img src={Pikachu} onMouseOver={this.MouseOver} onMouseOut={this.MouseOut} />
-
-            </div>
+        <div className="h-full w-1/2 flex-col flex select-none" id="rightside">
+          <div className="my-auto ml-12">
+              <Prizes />
+          </div>
         </div>
       </section>
+
       </Parallax>
+      </Parallax>
+      
     );
   }
 }
