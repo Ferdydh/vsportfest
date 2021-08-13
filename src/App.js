1b0c877722bb3eebe668bde27d9d97f1b0e99836
crustypluto19 <evanchristopher1907@gmail.com>
2021-08-14 00:09:50 +0200

    blah

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import Pubg from './scripts/Pubg'
 import Dance from './scripts/Dance'
 import VirtualRun from './scripts/VirtualRun';
 import Chess from './scripts/Chess';
+import Content from './scripts/Content'
 
 function App() {
   return (
