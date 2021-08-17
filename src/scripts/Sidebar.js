925f665490759dca9bb8532d21973c7f982cc37a
crustypluto19 <evanchristopher1907@gmail.com>
2021-08-17 14:51:35 +0200

    changes

--- src/scripts/Sidebar.js
+++ src/scripts/Sidebar.js
@@ -106,7 +106,7 @@ class Sidebar extends React.Component {
 
 
       {isMobile && is_visible && (<Menu as="div" className="inline-block text-left fixed mobilemenu">
-        <div>
+        <div className="p-4 mt-10">
           <Menu.Button className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-2 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500">
             <ChevronDownIcon className="h-5 w-5" aria-hidden="true" />
           </Menu.Button>
