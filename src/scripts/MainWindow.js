3dba98d6cdbc8dd28185d03af486569e9a12fe91
crustypluto19 <evanchristopher1907@gmail.com>
2021-08-20 20:46:49 +0200

    style changes

--- src/scripts/MainWindow.js
+++ src/scripts/MainWindow.js
@@ -7,13 +7,12 @@ import InstagramIcon from '@material-ui/icons/Instagram';
 import Countdown from './Countdown'
 import EmailIcon from '@material-ui/icons/Email';
 import WhatsApp from '@material-ui/icons/WhatsApp';
-import PIC from '../images/Cover/CHESS.jpg';
-import CHESS_COVER from '../images/Cover/CHESS.jpg';
-import DANCE_COVER from '../images/Cover/DANCE.jpg';
-import DOTA_COVER from '../images/Cover/DOTA.jpg';
-import MOLEG_COVER from '../images/Cover/MOBILELEGENDS.jpg';
-import PUBG_COVER from '../images/Cover/PUBG.jpg';
-import RUN_COVER from '../images/Cover/RUN.jpg';
+import CHESS_COVER from '../images/CompressedImages/CHESSSMALL.png';
+import DANCE_COVER from '../images/CompressedImages/DANCESMALL.png'
+import DOTA_COVER from '../images/CompressedImages/DOTASMALL.png'
+import MOLEG_COVER from '../images/CompressedImages/MOLEGSMALL.png'
+import PUBG_COVER from '../images/CompressedImages/PUBGSMALL.png'
+import RUN_COVER from '../images/CompressedImages/RUNSMALL.png'
 import {isMobile} from 'react-device-detect';
 import ReactPlayer from "react-player";
 import pdf from "../Guideline.pdf"
@@ -23,22 +22,7 @@ import '../index.css'
 
 export default class MainWindow extends React.Component {
     
-    MouseOut(event) {
-        event.target.style.height = '17rem'
-        event.target.style.width = '11.22rem'
-      }
-    
-      MouseOver(event){
-        if (!isMobile) {
-            event.target.style.height = '20rem'
-            event.target.style.width = '13rem'    
-        } else {
-            event.target.style.height = '18.5rem'
-            event.target.style.width = '12rem'        
-        }
-        event.target.style.transition = 'height 0.3s, width 0.3s, transform 0.3s';
-      }
-    
+
     render() {
         return (
             <section id="mainwindow" className="pt-6 pb-20 min-h-screen w-full bg-gradient-to-b to-purple-800 via-blue-900 from-pink-500">
@@ -69,10 +53,10 @@ export default class MainWindow extends React.Component {
                         Event <br></br>7 Okt - 10 Okt
                 </div>
 
-                <div className=" flex-1 flex justify-center w-auto h-1/2 relative player-wrapper mx-4 mb-14 md:pb-8">
+                <div className="flex-1 flex justify-center w-auto max-h-48 relative player-wrapper mx-4 mb-10 md:mb-0">
                     <ReactPlayer
                         url="https://www.youtube.com/watch?v=qGWsNJ2zF4Q"
-                        playing={true}
+                        playing={false}
                         className="top-0 left-1/2 transform -translate-x-1/2 absolute"
                         width= {!isMobile?"75%":"100%"}
                         height= {!isMobile?"75%":"100%"}
@@ -81,11 +65,6 @@ export default class MainWindow extends React.Component {
                 </div>
 
                 <div id="SelectionDiv" className="flex-wrap flex-row flex justify-center"> 
-                    
-                    <div>
-
-                    </div>
-
                     <Link
                         activeClass="active"
                         to="dota"
@@ -94,7 +73,7 @@ export default class MainWindow extends React.Component {
                         duration={500}
                     >
                         <div>
-                            <img src={MOLEG_COVER} className = "gamecover p-2" onMouseOver={this.MouseOver} onMouseOut={this.MouseOut}/>
+                            <img src={MOLEG_COVER} className = "hover:scale-125 transition duration-700 transform w-40 p-4"/>
                             <div className="relative text-2xl font-thin text-white">
                                 MOLEG
                             </div>
@@ -110,7 +89,7 @@ export default class MainWindow extends React.Component {
                         duration={500}
                     >
                         <div>
-                            <img src={PUBG_COVER} className = "gamecover p-2" onMouseOver={this.MouseOver} onMouseOut={this.MouseOut}/>
+                            <img src={PUBG_COVER} className = "hover:scale-125 transition duration-700 transform w-40 p-4"/>
                             <div className="relative text-2xl font-thin text-white">
                                PUBG
                             </div>
@@ -125,7 +104,7 @@ export default class MainWindow extends React.Component {
                         duration={500}
                     >
                         <div>
-                            <img src={RUN_COVER} className = "gamecover p-2" onMouseOver={this.MouseOver} onMouseOut={this.MouseOut}/>
+                            <img src={RUN_COVER} className = "hover:scale-125 transition duration-700 transform w-40 p-4"/>
                             <div className="relative text-2xl font-thin text-white">
                                 RUN
                             </div>
@@ -140,7 +119,7 @@ export default class MainWindow extends React.Component {
                         duration={500}
                     >
                         <div>
-                            <img src={CHESS_COVER} className = "gamecover p-2" onMouseOver={this.MouseOver} onMouseOut={this.MouseOut}/>
+                            <img src={CHESS_COVER} className = "hover:scale-125 transition duration-700 transform w-40 p-4"/>
                             <div className="relative text-2xl font-thin text-white">
                                 CHESS
                             </div>
@@ -155,7 +134,7 @@ export default class MainWindow extends React.Component {
                         duration={500}
                     >
                         <div>
-                            <img src={DANCE_COVER} className = "gamecover p-2" onMouseOver={this.MouseOver} onMouseOut={this.MouseOut}/>
+                            <img src={DANCE_COVER} className = "hover:scale-125 transition duration-700 transform w-40 p-4"/>
                             <div className="relative text-2xl font-thin text-white">
                                 DANCE
                             </div>
@@ -170,27 +149,28 @@ export default class MainWindow extends React.Component {
                         duration={500}
                     >
                         <div>
-                            <img src={DOTA_COVER} className = "gamecover p-2" onMouseOver={this.MouseOver} onMouseOut={this.MouseOut}/>
+                            <img src={DOTA_COVER} className = "hover:scale-125 transition duration-700 transform w-40 p-4"/>
                             <div className="relative text-2xl font-thin text-white">
                                 DOTA 2
                             </div>
                         </div>
                     </Link>
-
-                    <div>
-                        <div>Learn More</div>
-                        <a href ={pdf} target = "_blank" className="">Download Pdf</a>
-                    </div>
-                    
                 </div>
-                
-                <div className="font-bold text-white text-3xl p-10 font-mono">
+                    
+                <div className="animate-pulse text-3xl p-10 pb-5 text-red-600 font-bold font-mono">
                     <Countdown />
                 </div>
 
-                <div className="flex-1 mr-auto text-3xl  md:text-5xl font-extrabold font-mono p-3 m-3 content-center justify-center rounded-lg items-center animate-pulse">
+                <a href ={pdf} target = "_blank" rel="noreferrer" className="flex align-middle justify-center content-center">
+                    <div className="font-mono text-2xl p-4 flex align-middle text-cyan hover:scale-110">
+                        Learn more
+                        <div className="mt-1"><span class="material-icons">chevron_right</span></div>
+                    </div>
+                </a>
+                
+                {/* <div className="flex-1 mr-auto text-3xl  md:text-5xl font-extrabold font-mono p-3 m-3 content-center justify-center rounded-lg items-center animate-pulse">
                     <a href="https://www.w3schools.com/" target="_blank" rel="noreferrer" className="p-3 rounded-xl"  style={{backgroundColor: "#EEC184"}}>Register now!</a>
-                </div>
+                </div> */}
             </section>
         );
     }
