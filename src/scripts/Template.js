7a410ab97bd7ee89295e8de69fb6fb58121696c1
Ferdyya <ferdyya@gmail.com>
2021-08-23 22:41:16 +0200

    working on template

--- src/scripts/Template.js
+++ src/scripts/Template.js
@@ -1,21 +1,13 @@
 import React from 'react';
-import Prizes from './Prizes';
-import { Parallax } from 'react-parallax';
 import Modal from './Modal';
-import RegistrationButton from './RegistrationButton'; 
+// import BG from '../images/chessbg.jpg'
 
 // media imports for section
-import FG from '../images/CompressedImages/MASCOT_MOLEG.png';
-import ICON from '../images/ml.png';
+// import ICON from '../images/dota.png';
+import GP from '../images/gameplay.png';
 import {isMobile} from 'react-device-detect';
 import IRING from '../images/CompressedImages/RINGS/IRING1.png'
 import ORING from '../images/CompressedImages/RINGS/ORING1.png'
-import MASCOT from '../images/CompressedImages/MASCOT_PUBG.png'
-
-// const prizes = `Prizes 🏆
-// 1st place: 80€ + Gold Medal 🥇 \n
-// 2nd place: 40€
-// `
 
 const products = ['1st place: 80€ + Gold Medal','2nd place: 40€'];
 
@@ -33,14 +25,8 @@ const text =  `Gamers everywhere gather ‘round! 🎮
 Don’t miss your chance to compete and have fun with players all across the world 🌎 
 so sign up now for our DOTA COMPETITION ‼️`;
 
-class Template extends React.Component {
-  constructor(props){
-    super(props);
-    // this.state.text = `Gamers everywhere gather ‘round! 🎮 
-    // Don’t miss your chance to compete and have fun with players all across the world 🌎 
-    // so sign up now for our DOTA COMPETITION ‼️`;
-  }
 
+class Template extends React.Component {
   MouseOver(event) {
     event.target.style.height = '120%';
     event.target.style.transform = 'rotate(20deg)'
@@ -53,73 +39,53 @@ class Template extends React.Component {
   }
 
   render() {
+    const logo = require('../images/SectionImages/dota.png');
     return (
-      <section id="Template" className="bg-purple-900 m-0 p-0 align-middle content-center justify-center min-h-screen w-screen flex flex-col" >
-        <div className=" h-1/4 bg-transparent ">
-          {/* TOP BORDER */}
+      <div className="w-screen">
+        <div>
+          <div className={"spacer spacerAspect relative flex justify-center content-center align-middle " + this.props.wave} />
         </div>
 
-        <div className=" h-2/4 w-screen flex flex-row">
+        <section id="dota" className="bg-purple-900 -mb-32 -mt-16 blur flex align-middle content-center justify-center flex-col w-screen" style={{backgroundColor: "#411582"}}>
+          <section className="sm:py-20 flex flex-col float-left sm:flex-row sm:ml-20">
+            {/* LEFT SIDE OF THE DIV */}
+            <div className="sm:h-full sm:w-1/2 flex flex-col sm:my-auto" id="leftside">
+              <div className="mt-20 sm:m-10 md:m-auto font-bold text-7xl">
+                <div className="absolute transform -translate-y-20">
+                  <img src={IRING} style={{"animation":"spin 5s linear infinite","animation-direction": "normal"}} className="h-80"></img>
+                </div>
 
-          {/* Left side */}
-          <div className="w-full z-10">
-            <div className="h-1/2 sm:h-full flex flex-1 flex-col z-10 justify-end mr-auto">
-              <div className="z-10">
-                <p className="text-base p-4 md:text-lg whitespace-normal text-white text-center font-sans bg-gray-300 
-                    bg-opacity-30 rounded hover:shadow-md m-10 mb-24 glow-cyan-xl z-20">
-                      <RegistrationButton 
-                        title="Dance" 
-                        url="https://docs.google.com/forms/d/e/1FAIpQLSdkBAeDrxeLgLYnJ7Lr6wevKaGH87U1VqUimbpVfw89DGyUxw/viewform"
-                      />
-                      Registration Fee: 69 €
-                </p>
+                <div className="absolute transform -translate-y-20">
+                  <img src={ORING} style={{"animation":"spin 7s linear infinite","animation-direction": "reverse"}} className="h-80"></img>
+                </div>
+
+                <div id="dotax" className="transform -skew-y-6 transition duration-500 ease-in-out hover:-translate-y-1 hover:scale-110 text-white select-none neonText">
+                  DOTA
+                </div>
+                  <Modal 
+                    title="Dota" 
+                    url="https://docs.google.com/forms/d/e/1FAIpQLSeCVJDz1oebmBpRbeb9aC4zsB0fRkI4M1cITGRmUE0W19q77A/viewform"
+                  />
               </div>
             </div>
-          </div>
-
-          {/* middle part */}
-          <div className="w-full flex align-middle justify-center flex-col">
-            <div className="font-white text-white text-center text-7xl font-bold z-10 flex flex-row align-middle justify-center">
-              <img src={ICON} className=""></img>
-            </div>
-            <div className="flex flex-row align-middle justify-center z-10">
-              <img src={MASCOT} className="h-40"></img>
-            </div>
-            <div class="flex flex-row align-middle justify-center">
-              <div className="absolute max-w-full text-base p-4 md:text-lg whitespace-normal text-white text-center font-sans bg-gray-300 
-                    bg-opacity-30 rounded hover:shadow-md m-10 mb-24 glow-cyan-xl select-none z-20">
-                {text}
-              </div>
-            </div>
-
-          </div>
-
-        {/* Right side */}
-          <div className="w-full bg-blue-500 ">
-            <div className="h-1/2 sm:h-full flex flex-col">
-              <div className="">
-                <p className="text-base p-4 md:text-lg whitespace-normal text-white text-center font-sans bg-gray-300 
+            {/* RIGHT SIDE OF THE DIV */}
+            <div className="h-1/2 sm:h-full sm:w-1/2 flex flex-col" id="rightside">
+              <div className="md:m-10 md:mt-36">
+                <p className="text-base p-4 md:text-lg max-w-md whitespace-normal text-white text-center font-sans bg-gray-300 
                     bg-opacity-30 rounded hover:shadow-md m-10 mb-24 glow-cyan-xl">
+                      <div>{text}</div>
+                      <hr/>
                       <div>Prizes 🏆</div>
-                      {prizes}
+                      <div>{prizes}</div>
                 </p>
               </div>
             </div>
-
-          </div>
-
-        </div>
-
-        <div className=" h-1/4 bg-transparent ">
-          {/* BOTTOM BORDER */}
-
-
-        </div>
-
-        <img src={IRING} className="absolute left-0 right-0 ml-auto mr-auto w-3/5 z-0" style={{"animation":"spin 100s linear infinite","animation-direction": "normal"}}></img>
-        <img src={ORING} className="absolute left-0 right-0 ml-auto mr-auto w-3/5 z-0" style={{"animation":"spin 4s linear infinite","animation-direction": "reverse"}}></img>
-
-      </section>
+          </section>
+      </section>  
+      <div className="flex-1 flex justify-center" style={{backgroundColor: "#411582"}}>
+        <img src={logo} className="h-80"></img>
+      </div>
+    </div>
     );
   }
 }
