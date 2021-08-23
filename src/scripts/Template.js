4cf7dcb5a9eeb7c784d4afc027d6dd816af8b161
alvandi <geradylanalvandi@gmail.com>
2021-08-23 02:27:04 +0200

    added spins

--- src/scripts/Template.js
+++ src/scripts/Template.js
@@ -28,7 +28,7 @@ class Template extends React.Component {
           {/* TOP BORDER */}
         </div>
 
-        <div className=" h-2/4 bg-white w-screen flex flex-row">
+        <div className=" h-2/4 w-screen flex flex-row">
 
           <div className="w-full bg-green-500 ">
             <div className="flex-1 flex justify-end mr-auto">
@@ -41,11 +41,11 @@ class Template extends React.Component {
             WUII
           </div>
 
-          <div className="w-full bg-purple-300 flex align-middle justify-center flex-col">
-            <div className="font-white text-white text-center text-7xl font-bold">
+          <div className="w-full flex align-middle justify-center flex-col">
+            <div className="font-white text-white text-center text-7xl font-bold z-10 ">
               MOBILE LEGENDS
             </div>
-            <div className="flex flex-row align-middle justify-center">
+            <div className="flex flex-row align-middle justify-center z-10">
             <img src={ICON} className=""></img>
             </div>
           </div>
@@ -60,7 +60,8 @@ class Template extends React.Component {
           {/* BOTTOM BORDER */}
         </div>
 
-        <img src={ICON} className="absolute left-0 right-0 ml-auto mr-auto w-80"></img>
+        <img src={ICON} className="absolute left-0 right-0 ml-auto mr-auto w-3/5 z-0" style={{"animation":"spin 5s linear infinite","animation-direction": "normal"}}></img>
+        <img src={ICON} className="absolute left-0 right-0 ml-auto mr-auto w-3/5 z-0" style={{"animation":"spin 3s linear infinite","animation-direction": "normal"}}></img>
 
       </section>
     );
